dea4e52094123bf60d4839ed3d28b6cad3c83512
Consep33t <agengp360@gmail.com>
2025-07-18 03:41:56 +0700

    fix bug irsensor

--- src/components/IRStatus.jsx
+++ src/components/IRStatus.jsx
@@ -3,9 +3,9 @@ function IRStatus({ ir1, ir2, ir3 }) {
     <div className="bg-white p-4 shadow rounded">
       <h2 className="text-lg font-semibold">IR Sensor</h2>
       <ul className="text-sm">
-        <li>Sensor-1: {ir1 ? "TERDETEKSI" : "TIDAK"}</li>
-        <li>Sensor-2: {ir2 ? "TERDETEKSI" : "TIDAK"}</li>
-        <li>Sensor-3: {ir3 ? "TERDETEKSI" : "TIDAK"}</li>
+        <li>Sensor-1: {ir1 == 1 ? "TERDETEKSI" : "TIDAK"}</li>
+        <li>Sensor-2: {ir2 == 1 ? "TERDETEKSI" : "TIDAK"}</li>
+        <li>Sensor-3: {ir3 == 1 ? "TERDETEKSI" : "TIDAK"}</li>
       </ul>
     </div>
   );
